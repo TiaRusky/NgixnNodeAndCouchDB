0a3329afa4f4350f28d9136d55cae2fa20d4264b
Mattia Russo <russo.1918968@studenti.uniroma1.it>
2022-03-20 13:24:46 +0100

    Now nodes update the info in the DB

--- node/index.js
+++ node/index.js
@@ -18,8 +18,25 @@ app.get("/",(req,res)=>{
             var info = JSON.parse(body);
             res.end("Ciao, sono il nodo "+nodo.toString()+"; le visite sono: "+info.count);
             const rev = info._rev;
-            var count = info.cont +1;
+            var count = info.count +1;
             console.log(rev+":"+count);
+            var data = {"_rev":rev,"count":count};
+            var bd = JSON.stringify(data);
+            const option = {
+                uri : "http://"+admin+":"+password+"@couchdb:5984/info/visite",
+                method: "PUT",
+                headers: { 'content-type':'application/json', 'content-lenght':+""+bd.length},
+                body: bd
+            }
+            //Updating the count
+            request(option,(error,respose,body)=>{
+                if(error){
+                    console.log("Erorre nell'update del documento: "+error);
+                }
+                else{
+                    console.log(body);
+                }
+            });
         }
     });
 });
