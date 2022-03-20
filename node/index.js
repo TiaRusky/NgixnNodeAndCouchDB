5c09d1e93d6352c96b4e6ee1f90ca7c1f5d82717
Mattia Russo <russo.1918968@studenti.uniroma1.it>
2022-03-20 12:47:17 +0100

    Now nodes access the couchdb

--- node/index.js
+++ node/index.js
@@ -1,10 +1,24 @@
 const express = require('express');
+const request = require('request');
 const PORT = 3000;
 const app = express();
 
+const admin = "mattia";
+const password = "password";
+
+//http://admin:password@couchdb:5984/mydb
+
 app.get("/",(req,res)=>{
-    const nodo = process.env.ISTANCE;
-    res.end("Ciao, sono il nodo "+nodo.toString());
+
+    var url = "http://"+admin+":"+password+"@couchdb:5984/info/visite"; //richiesta per il documento visite nel db info
+    request(url,(error,response,body)=>{
+        if(error)console.log("Errore nella request del documento visite: "+error);
+        else{
+            const nodo = process.env.ISTANCE;
+            var info = JSON.parse(body);
+            res.end("Ciao, sono il nodo "+nodo.toString()+"; le visite sono: "+info.count);
+        }
+    });
 });
 
 app.listen(PORT,()=>{
